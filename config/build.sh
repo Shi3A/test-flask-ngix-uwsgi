d5de296cafd53dbefc3e9b22d3b7170318305f07
Vincent <vincent@vincentdavis.net>
2016-01-28 09:07:17 -0700

    Update and rename notes.md to build.sh

---
+++ config/build.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install aptitude
+sudo aptitude -y install build-essential
+
+####
+# Download and install miniconda
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+Miniconda3-latest-Linux-x86_64.sh
+# This is the virtual env to be used by the flask app
+conda create -n testenv python=3.5 flask virtualenv uwsgi
