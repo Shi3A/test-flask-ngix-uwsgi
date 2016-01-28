3833553a12163918d7875a940067d5cde0629faa
Vincent <vincent@vincentdavis.net>
2016-01-28 09:10:46 -0700

    Update build.sh

--- config/build.sh
+++ config/build.sh
@@ -9,3 +9,6 @@ wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 Miniconda3-latest-Linux-x86_64.sh
 # This is the virtual env to be used by the flask app
 conda create -n testenv python=3.5 flask virtualenv uwsgi
+
+###
+# To be completed.
