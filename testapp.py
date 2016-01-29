34f60357b440e0a7b275557cb902721442e76536
Vincent Davis <vincent@heteroskedastic.com>
2016-01-29 10:38:25 -0700

    view return python version, file, path info.

--- testapp.py
+++ testapp.py
@@ -1,12 +1,16 @@
 from flask import Flask
+import sys, inspect, os
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
-
+    ver = str(sys.version_info)
+    insp = str(inspect.getfile(inspect.currentframe()))
+    fpath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    page = ('Hello World! <br> {} <br> {} <br> {}'.format(ver, insp, fpath))
+    return page
 
 if __name__ == '__main__':
     app.run()
